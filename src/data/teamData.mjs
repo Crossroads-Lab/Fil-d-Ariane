5d38dd960b564c95f8f64a1ab436e62f9905b1f2
William Brendel <brendel.william@gmail.com>
2024-07-09 11:37:40 -0700

    first commit

--- src/data/teamData.mjs
+++ src/data/teamData.mjs
@@ -1,20 +1,21 @@
 export const TEAM = [
   {
+    name: 'Pierrick JOLIVEAU',
+    src: 'assets/team/pierrick.webp',
+    title: 'General Partner | CEO',
+    linkedin: 'https://www.linkedin.com/in/pierrickjoliveau/'
+  },
+  {
     name: 'William Brendel',
     src: 'assets/team/will.webp',
-    title: 'General Partner',
+    title: 'General Partner | CTO, CPO',
     linkedin: 'https://www.linkedin.com/in/william-brendel/'
   },
   {
-    name: 'Tristan Perera',
-    src: 'assets/team/tristan.webp',
-    title: 'General Partner',
-    linkedin: 'https://www.linkedin.com/in/tristan-perera/'
-  },
-  {
-    name: 'Tharanga Mahavila',
-    src: 'assets/team/tharanga.webp',
-    title: 'Lead Engineer'
+    name: 'Pauline SAUVAGE',
+    src: 'assets/team/pauline.webp',
+    title: 'General Partner | CFO',
+    linkedin: 'https://www.linkedin.com/in/paulinesauvage/'
   }
 ];
 
